5b2ba56b9b4367e9d9af867855e6d85cc6e9a89d
Guan <20001117@qq.com>
2019-07-02 00:01:29 +0800

    first time

---
+++ src/main/java/com/tester/model/GetUserInfoCase.java
@@ -0,0 +1,25 @@
+package com.tester.model;
+
+import lombok.Data;
+
+@Data
+public class GetUserInfoCase {
+    private int userId;
+    private String expected;
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public String getExpected() {
+        return expected;
+    }
+
+    public void setExpected(String expected) {
+        this.expected = expected;
+    }
+}
---
+++ src/main/java/com/tester/model/LoginCase.java
@@ -0,0 +1,43 @@
+package com.tester.model;
+
+import lombok.Data;
+
+@Data
+public class LoginCase {
+        private int id;
+        private String userName;
+        private String password;
+        private String expected;
+
+        public int getId() {
+                return id;
+        }
+
+        public void setId(int id) {
+                this.id = id;
+        }
+
+        public String getUserName() {
+                return userName;
+        }
+
+        public void setUserName(String userName) {
+                this.userName = userName;
+        }
+
+        public String getPassword() {
+                return password;
+        }
+
+        public void setPassword(String password) {
+                this.password = password;
+        }
+
+        public String getExpected() {
+                return expected;
+        }
+
+        public void setExpected(String expected) {
+                this.expected = expected;
+        }
+}
